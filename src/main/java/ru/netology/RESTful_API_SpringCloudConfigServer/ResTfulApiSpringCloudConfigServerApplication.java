4e1a536a4a1831396cdfb8c6fa5556291f279374
Gennady <g.korolew@gmail.com>
2025-03-20 20:41:03 +0400

    Initial commit

---
+++ src/main/java/ru/netology/RESTful_API_SpringCloudConfigServer/ResTfulApiSpringCloudConfigServerApplication.java
@@ -0,0 +1,16 @@
+package ru.netology.RESTful_API_SpringCloudConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+//Config Server, читает настройки из реп-я и отдавает их клиентам
+@EnableConfigServer
+public class ResTfulApiSpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulApiSpringCloudConfigServerApplication.class, args);
+	}
+
+}
